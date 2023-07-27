b6a818a3e1dc1b13dab5135d597212302f6dd4a6
Yulia Azovtseva <yulia.pryania@gmail.com>
2023-07-27 16:32:03 +0400

    comments added

--- src/test/java/SelenideGitHubTest.java
+++ src/test/java/SelenideGitHubTest.java
@@ -22,8 +22,10 @@ public class SelenideGitHubTest {
 
         open("");
 
+        // Ссылка, которую мы хотим найти на странице Wiki
         String linkToFind = "SoftAssertions";
 
+        // Текст, который мы хотим найти на странице "SoftAssertions" в Wiki
         String textToFind = """ 
                 Using JUnit5 extend test class:
                 @ExtendWith({SoftAssertsExtension.class})
@@ -31,13 +33,19 @@ public class SelenideGitHubTest {
                     @Test
                  """;
 
+        // Клик на меню для перехода на страницу Wiki
         $("#wiki-tab").click();
 
+        // Клик на ссылку "More Pages", чтобы раскрыть список ссылок Wiki страниц
         $(".wiki-more-pages-link button").click();
+
+        // Проверяем, есть ли нужная ссылка в списке Wiki страниц
         $("#wiki-pages-box").shouldHave(text(linkToFind));
 
+        // Клик на ссылку для перехода на страницу "SoftAssertions"
         $("#wiki-pages-box [href*=" + linkToFind + "]").click();
 
+        // Проверяем, есть ли на странице текст для JUnit5
         $("#wiki-body").shouldHave(text(textToFind));
 
     }
